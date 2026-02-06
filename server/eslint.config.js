6ea1e2eb351338d00e25ad6f8e19f31249cdb7bc
azamatalt <kalyan.number1@gmail.com>
2026-02-06 13:24:04 +0300

    25: Implement and configure ESLint, Prettier, and Husky for code quality and formatting.

---
+++ server/eslint.config.js
@@ -0,0 +1,33 @@
+const js = require('@eslint/js');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...tsPlugin.configs['recommended-type-checked'].rules,
+    },
+  },
+  {
+    rules: {
+      ...require('eslint-config-prettier'),
+    },
+  },
+];
